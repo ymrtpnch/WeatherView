d4923a28d9cd5527530a77a56d83e4660b52eac1
Artem Panchenko <Ymrtpnch@gmail.com>
2023-01-16 00:49:54 +0600

    Initial Commit

---
+++ WeatherTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTableView
+//
+//  Created by Артем Панченко on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
